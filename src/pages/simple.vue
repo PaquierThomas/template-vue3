fb68ebe0497114bb455e4860cd93ea1d8ec4cef8
PaquierThomas <95091416+PaquierThomas@users.noreply.github.com>
2022-09-09 10:43:20 +0200

    simple tableau const maisons

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -7,14 +7,37 @@
         image: "/public/pexels-binyamin-mellish-106399.jpg",
         lit: "5",
         nbrSDB: "2",
+        taille: "320",
 
-    }
+    };
+    const maison2 = {
+        nom: "Beverly Hils",
+        prix: "3200",
+        favori: false,
+        image: "/public/pexels-binyamin-mellish-106399.jpg",
+        lit: "4",
+        nbrSDB: "1",
+        taille: "212",
+
+    };
+    const maison3 = {
+        nom: "NewHope Ohio",
+        prix: "12300",
+        favori: true,
+        image: "/public/pexels-binyamin-mellish-106399.jpg",
+        lit: "7",
+        nbrSDB: "4",
+        taille: "560",
+
+    };
+
+    const maisons = [ {maison1}, {maison2}, {maison3},];
 </script>
 <template>
     <div class="p-2">
         <h2>Page maisons simple</h2>
         <Card/>
-        <Card v-bind="{nom: 'Beverly Springfield' , prix: 5000 , favori: true , image: '/public/pexels-binyamin-mellish-106399.jpg' , nbrSDB: 2 }"></Card>
+        <Card v-bind="{nom: 'Beverly Springfield' , prix: 5000 , favori: true , image: '/public/pexels-binyamin-mellish-106399.jpg' ,lit:4, nbrSDB: 2, taille: 125  }"></Card>
         <Card v-bind="maison1"/>
     </div>
 </template>
