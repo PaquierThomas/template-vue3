9a80fd9d4cdf4b788eb60ec8307f3976453586bb
PaquierThomas <95091416+PaquierThomas@users.noreply.github.com>
2022-09-09 09:57:07 +0200

    Etape2

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,23 @@
           <code class="font-mono">/src/pages/index.vue</code>
         </router-link>
       </li>
+      <li>
+        <router-link class="text-red-600 underline" to="simple">
+          lien vers
+          <code class="font-mono">/src/pages/simple.vue</code>
+        </router-link>
+      </li>
     </ul>
   </nav>
 
   <!-- Affiche les pages -->
   <router-view class="m-2 border-2 p-2" />
+
+  <card nom="test" :prix="34" favori image="" nbrSDB="5"/>
+  
 </template>
 
 <script setup lang="ts">
 import { Bars3Icon } from '@heroicons/vue/20/solid'
+
 </script>
---
+++ src/pages/simple.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import Card from "../components/card.vue" ;
+    const maison1 = {
+        nom: "Beverly Springfield",
+        prix: "5000",
+        favori: true,
+        image: "/public/pexels-binyamin-mellish-106399.jpg",
+        beds: ""
+        nbrSDB: "2"
+
+    }
+</script>
+<template>
+    <div class="p-2">
+        <h2>page simple</h2>
+        <Card v-bind="{nom: 'Beverly Springfield' , prix: 5000 , favori: true , image: '/public/pexels-binyamin-mellish-106399.jpg' , nbrSDB: 2 }"></Card>
+        <Card v-bind="maison1"/>
+    </div>
+</template>
