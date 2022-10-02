67fd25bade7c60f05878cb73f0a8aca6709ccf7c
PaquierThomas <95091416+PaquierThomas@users.noreply.github.com>
2022-10-02 16:09:39 +0200

    réparation liste fetch

--- src/pages/liste-fetch.vue
+++ src/pages/liste-fetch.vue
@@ -1,10 +1,14 @@
-<script setup>
-const res = await fetch("/src/maison.json");
-const maisons = await res.json();
+<script setup lang="ts">
+    import Card from "../components/card.vue"
 
-import Card from "../components/card.vue" ;
+    const res = await fetch("../../public/maison.json")
+
+    const maisons = await res.json()
 </script>
 
 <template>
-    <Card v-for="m in maisons" :key="m.nom" v-bind="m"/>
+    <div>
+        <h2>Liste en utilisant fetch</h2>
+        <Card v-for="m in maisons" :key="m.nom" v-bind="m"/>
+    </div>
 </template>
---
+++ src/pages/quartier/index.vue
@@ -0,0 +1,12 @@
+<script>
+import groupBy from "lodash/groupBy";
+</script>
+
+<Disclosure
+    v-for="(listeQuartier, libelleCommune) in groupBy(
+      data,
+      'libelle_Commune'
+    )"
+    :key="libelleCommune"
+  >
+
